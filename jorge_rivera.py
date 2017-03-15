f687ce16763cfac77bf826d0d184d1c2509a512a
Jorge Rivera <acidsnakerj@gmail.com>
2017-03-14 23:24:26 -0500

    Exercise 2 complete

--- jorge_rivera.py
+++ jorge_rivera.py
@@ -1,8 +1,10 @@
 
 def approved():
+    
     """
         Funcion que determina si un estudiante aprueba una materia por asistencia o no.
-        
+        Desactivar el codigo comentado apartir de la linea 16 hasta 23 para usar la funcion 
+        ingresando datos, de igual manera se recomienda comentar las lineas 11 y 13 respectivamente
     """
 
     notes = [3.5, 4.0, 5.0]
@@ -21,7 +23,7 @@ def approved():
         assistence.append(int(note))
         cont += 1
     """
-    prom = notes[0] + notes[1] + notes[2]) / len(notes
+    prom = notes[0] + notes[1] + notes[2] / len(notes)
     if prom < 3.0:
         print 'Perdio la materia, no cumple con el promedio minimo para aprobar.'
     else:
@@ -34,4 +36,23 @@ def approved():
         else:
             print 'Infortunadamente ha perdido por fallas, su promedio fue %d' % prom
 
-approved()
+def high_low_number():
+    """
+        Funcion que determina que cual de los dos numeros es mayor, de igual manera 
+        si se desea digitar manualmente los numeros quitar comentarios en la linea 48 a la 53
+        y comentar la lista numbers prestablecida
+    """
+    numbers = [1, 4]
+    # numbers = []
+    i = 0
+    """
+    while i < 2:
+        message = 'Escriba el numero: '
+        num = int(input(message))
+        numbers.append(num)
+        i += 1
+    """        
+    if numbers[0] < numbers[1]:
+        print 'El numero %d es mayor que el numero %d' % (numbers[1], numbers[0])
+    else:
+        print 'El numero %d es mayor que el numero %d' % (numbers[0], numbers[1])
